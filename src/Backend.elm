5f3458bd43bd1960633f5f6b7b5cbc0ade54d7d5
franpassa <francopassarelli94@gmail.com>
2019-08-07 20:14:48 -0300

    update Bckend.elm

--- src/Backend.elm
+++ src/Backend.elm
@@ -20,6 +20,7 @@ import Models exposing (Model)
 
 -- Debería darnos la url de la cancion en base al id
 urlById : String -> List Song -> String
+--urlById id songs = ""
 urlById idSearched songs =  (searchById idSearched songs).url
 
 sameId : String -> Song -> Bool
@@ -31,17 +32,27 @@ searchById idSearched songs = findSong (sameId idSearched) songs
 -- Debería darnos las canciones que tengan ese texto en nombre o artista
 
 filterByName : String -> List Song -> List Song
-filterByName text songs = filter (sameNameOrArtist text) songs
+--filterByName text songs = songs
+filterByName text songs = List.filter (sameNameOrArtist (String.toLower text)) (List.map String.toLower songs)
 
 sameNameOrArtist : String -> Song -> Bool
-sameNameOrArtist text song = (text == song.name) || (text == song.artist)
+sameNameOrArtist text song = String.contains text song.artist ||  String.contains text song.artist
 
 -- Recibe un id y tiene que likear/dislikear una cancion
 -- switchear song.liked
 
 toggleLike : String -> List Song -> List Song
-toggleLike id songs = (searchById id songs).liked => (searchById id songs).liked = False
-                   |= (searchById id songs).liked = True
+--toggleLike id songs = songs
+toggleLike id songs = List.map (switchById id) songs
+
+switchById : String -> Song -> Song
+switchById id song = if (sameId id song) then (switchLike song) else song
+
+switchLike : Song -> Song
+switchLike song = if isLiked song then { song | liked = False } else { song | liked = True }
+
+--(searchById id songs).liked => (searchById id songs).liked = False
+--|= (searchById id songs).liked = True
 
 --if (searchById id songs).liked == True then (searchById id songs).liked = False else (searchById id songs).liked = True 
     
@@ -50,28 +61,38 @@ toggleLike id songs = (searchById id songs).liked => (searchById id songs).liked
 -- hay que arreglarla
 
 isLiked : Song  -> Bool
+--isLiked song = False
 isLiked song = song.liked
 
 -- Recibe una lista de canciones y nos quedamos solo con las que
 -- tienen un like
 
 filterLiked : List Song -> List Song
-filterLiked songs = filter isLiked songs
+--filterLiked songs = songs
+filterLiked songs = List.filter isLiked songs
 
 -- Agrega una cancion a la cola de reproduccion
 -- (NO es necesario preocuparse porque este una sola vez)
 
 addSongToQueue : Song -> List Song -> List Song
-addSongToQueue song queue = queue
+--addSongToQueue song queue = queue
+addSongToQueue song queue = song::queue
 
 -- Saca una cancion de la cola
 -- (NO es necesario que se elimine una sola vez si esta repetida)
+
 removeSongFromQueue : String -> List Song -> List Song
-removeSongFromQueue id queue = queue
+--removeSongFromQueue id queue = queue
+removeSongFromQueue id queue = List.filter (diferentId id) queue
+
+diferentId : String -> Song -> Bool
+diferentId idSearched song = not (sameId idSearched song)
 
 -- Hace que se reproduzca la canción que sigue y la saca de la cola
+
 playNextFromQueue : Model -> Model
 playNextFromQueue model = model
+--playNextFromQueue model = nextQueue model 
 
 -------- Funciones Listas --------
 
